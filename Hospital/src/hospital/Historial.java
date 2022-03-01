fa9e96c71686950208f603f26cd2bfa2fc32bfe9
AndresFChica <andresfchicag60@gmail.com>
2022-02-28 20:19:41 -0500

    Cambio en atributos historial

--- Hospital/src/hospital/Historial.java
+++ Hospital/src/hospital/Historial.java
@@ -4,12 +4,13 @@ public class Historial {
 	private String[] enfermedades;
 	private String[] operaciones;
 	private String[] alergias;
-	//private Cita cita;
+	private Cita cita;
 	
-	public Historial(String[] enfermedades, String[] operaciones, String[] alergias) {
+	public Historial(String[] enfermedades, String[] operaciones, String[] alergias, Cita cita) {
 		super();
 		this.enfermedades = enfermedades;
 		this.operaciones = operaciones;
 		this.alergias = alergias;
+		this.cita = cita;
 	}
 }
