51eb7e06c44f66ec208cf7b8b6ee396b5376ac69
LorenzoPorras <lpddeadblack1@gmail.com>
2022-03-04 14:58:51 -0500

    arreglando cosas
    Edtoy cambiando un poco la logica de los metodos

--- Hospital/src/hospital/Sala.java
+++ Hospital/src/hospital/Sala.java
@@ -2,7 +2,6 @@ package hospital;
 
 import java.util.*;
 
-
 public class Sala {
 
 	// Cambie capacidad de un String a un int y descomente los pacientes. Creo que
@@ -14,43 +13,6 @@ public class Sala {
 	private Paciente pacientes[];
 	private Enfermero enfermeros[];
 
-	public boolean verificarPaciente(String cedula) {
-		int i = 0;
-		while (i < pacientes.length) {
-			if (cedula == pacientes[i].getCC()) {
-				return true;
-				// porque si existe un paciente con esa cedula
-			}
-		}
-		return false; // returns false porque no existe } // agregar exepcion si el paciente ya existe
-						// public void
-
-	}
-
-	public void addPaciente(Paciente paciente, String nombre, String apellido, String cc, String poliza,
-			String[] sintomas, int triaje, String[] acompanantes, int edad, String sexo, String tipoSangre,
-			Historial historial) {
-		// traer cedula de paciente. //Verificarla en un metodo contra un arreglo de
-		// pacientes //En caso de que la cedula no exista, agregar el paciente
-		if (verificarPaciente(paciente.getCC()) == false) {
-			pacientes = Arrays.copyOf(pacientes, pacientes.length + 1);
-			pacientes[pacientes.length - 1] = new Paciente(nombre, apellido, cc, poliza, sintomas, triaje, acompanantes,
-					edad, sexo, tipoSangre, historial);
-		}
-	}
-
-	public boolean verificarEquipo(String codigo) {
-		int i = 0;
-		while (i < equipos.length) {
-			if (codigo == equipos[i].getCodigo()) {
-				return true;
-				// returns true porque si existe
-			}
-		}
-		return false;
-		// returns false porque no existe
-	}
-
 	public String getTipo() {
 		return tipo;
 	}
@@ -59,15 +21,66 @@ public class Sala {
 		this.capacidad = capacidad;
 	}
 
-	// agregar exepcion si el equipo ya existe
-	public void addEquipo(Equipo equipo, double inventario, boolean disponibilidad, String codigo, boolean estado) {
-		if (verificarEquipo(equipo.getCodigo()) == false) {
-			equipos = Arrays.copyOf(equipos, equipos.length + 1);
-			equipos[equipos.length - 1] = new Equipo(inventario, disponibilidad, codigo, estado);
+	// Metodos de busqueda qeu devuelven la posicion-------------------------
 
+	public int verificarPaciente(String cedula) {
+		int i = 0;
+		while (i < pacientes.length) {
+			if (cedula == pacientes[i].getCC()) {
+				return i;
+			}
 		}
+		return 0;
 	}
 
+	public int verificarEquipo(String codigo) {
+		int i = 0;
+		while (i < equipos.length) {
+			if (codigo == equipos[i].getCodigo()) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	public int verificarMedicamento(String id) {
+		int i = 0;
+		while (i < medicamentos.length) {
+			if (id == medicamentos[i].getId()) {
+				return i;
+			}
+		}
+		return 0;
+	}
+	// Metodos------------------------------------
+
+	public void addPaciente(Paciente paciente, String nombre, String apellido, String cc, String poliza,
+			String[] sintomas, int triaje, String[] acompanantes, int edad, String sexo, String tipoSangre,
+			Historial historial) {
+		// traer cedula de paciente. //Verificarla en un metodo contra un arreglo de
+		// pacientes //En caso de que la cedula no exista, agregar el paciente
+		if (verificarPaciente(paciente.getCC()) != 0) {
+			pacientes = Arrays.copyOf(pacientes, pacientes.length + 1);
+			pacientes[pacientes.length - 1] = new Paciente(nombre, apellido, cc, poliza, sintomas, triaje, acompanantes,
+					edad, sexo, tipoSangre, historial);
+		}
+	}
+	// agregar exepcion si el equipo ya existe
+	public void addEquipo(Equipo equipo, double inventario, boolean disponibilidad, String codigo, boolean estado) {
+		if (verificarEquipo(equipo.getCodigo()) != 0) {
+			equipos = Arrays.copyOf(equipos, equipos.length + 1);
+			equipos[equipos.length - 1] = new Equipo(inventario, disponibilidad, codigo, estado);
+		}
+	}
+	// agregar exepcion si el medicamento ya existe
+	public void addMedicamento(Medicamento medicamento, String nombre, String id, Date fechaVencimiento,
+			Date fechaCompra, boolean disponibilidad, double cantidad) {
+		if (verificarMedicamento(medicamento.getId()) != 0) {
+			medicamentos = Arrays.copyOf(medicamentos, medicamentos.length + 1);
+			medicamentos[medicamentos.length - 1] = new Medicamento(nombre, id, fechaVencimiento, fechaCompra,
+					disponibilidad, cantidad);
+		}
+	}
 	public void eliminarEquipo(String codigoEquipo) {
 		int i = 0;
 		while (i < equipos.length) {
@@ -78,30 +91,6 @@ public class Sala {
 			}
 		}
 	}
-
-	public boolean verificarMedicamento(String id) {
-		int i = 0;
-		while (i < medicamentos.length) {
-			if (id == medicamentos[i].getId()) {
-				return true;
-				// returns true porque si existe
-			}
-		}
-		return false;
-		// returns false porque no existe
-	}
-
-	// agregar exepcion si el medicamento ya existe
-	public void addMedicamento(Medicamento medicamento, String nombre, String id, Date fechaVencimiento,
-			Date fechaCompra, boolean disponibilidad, double cantidad) {
-		if (verificarMedicamento(medicamento.getId()) == false) {
-			medicamentos = Arrays.copyOf(medicamentos, medicamentos.length + 1);
-			medicamentos[medicamentos.length - 1] = new Medicamento(nombre, id, fechaVencimiento, fechaCompra,
-					disponibilidad, cantidad);
-
-		}
-	}
-
 	public void eliminarMedicamento(String idMedicamento) {
 		int i = 0;
 		while (i < medicamentos.length) {
