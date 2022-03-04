261d2725c0380b235e59034fa196e8e88361bc52
rafaelarango66 <rafael.arango66@eia.edu.co>
2022-03-04 16:21:11 -0500

    CreacionEnfermero(add,eliminar,verificar) y arreglos class sala

--- Hospital/src/hospital/Sala.java
+++ Hospital/src/hospital/Sala.java
@@ -13,7 +13,38 @@ public class Sala {
 	private Equipo equipos[];
 	private Paciente pacientes[];
 	private Enfermero enfermeros[];
+	
+	public boolean verificarEnfermero(String cedula) {
+		int i = 0;
+		while (i < enfermeros.length) {
+			if (cedula == enfermeros[i].getCC()) {
+				return true;
+				// porque si existe un paciente con esa cedula
+			}
+		}
+		return false; 
+	}
 
+	public void addEnfermero(String nombre, String apellido, String CC, boolean disponible) {
+		if(verificarEnfermero(CC)==false) {
+			enfermeros = Arrays.copyOf(enfermeros, enfermeros.length+1);
+			enfermeros[enfermeros.length-1] = new Enfermero(nombre,apellido,CC,disponible);
+		}
+		//else el enfermero ya existe entonces no se puede agregar
+	}
+	
+	public void eliminarEnfermero(String CC) {
+		if(verificarEnfermero(CC)== true) {
+			int i= 0;
+			while(i<enfermeros.length) {
+				if(enfermeros[i].getCC()==CC) {
+					enfermeros[enfermeros.length-1]=enfermeros[i];
+					enfermeros = Arrays.copyOf(enfermeros, enfermeros.length-1);
+				}
+			}
+		}
+		//si false, no existe un enfermero con esa cedula para eliminar ent exepction
+	}
 	public boolean verificarPaciente(String cedula) {
 		int i = 0;
 		while (i < pacientes.length) {
@@ -26,12 +57,12 @@ public class Sala {
 						// public void
 	}
 
-	public void addPaciente(Paciente paciente, String nombre, String apellido, String cc, String poliza,
+	public void addPaciente(String nombre, String apellido, String cc, String poliza,
 			String[] sintomas, int triaje, String[] acompanantes, int edad, String sexo, String tipoSangre,
 			Historial historial) {
 		// traer cedula de paciente. //Verificarla en un metodo contra un arreglo de
 		// pacientes //En caso de que la cedula no exista, agregar el paciente
-		if (verificarPaciente(paciente.getCC()) == false) {
+		if (verificarPaciente(cc) == false) {
 			pacientes = Arrays.copyOf(pacientes, pacientes.length + 1);
 			pacientes[pacientes.length - 1] = new Paciente(nombre, apellido, cc, poliza, sintomas, triaje, acompanantes,
 					edad, sexo, tipoSangre, historial);
@@ -48,8 +79,9 @@ public class Sala {
 				}
 				i++;
 			}
-			//No se encontro paciente con esa cedula. Ent no se puede eliminar
+			
 		}
+		//Si false no se encontro paciente con esa cedula. Ent no se puede eliminar
 	}
 	public boolean verificarEquipo(String codigo) {
 		int i = 0;
@@ -72,15 +104,17 @@ public class Sala {
 	}
 
 	// agregar exepcion si el equipo ya existe
-	public void addEquipo(Equipo equipo, double inventario, boolean disponibilidad, String codigo, boolean estado) {
-		if (verificarEquipo(equipo.getCodigo()) == false) {
+	public void addEquipo(double inventario, boolean disponibilidad, String codigo, boolean estado) {
+		if (verificarEquipo(codigo) == false) {
 			equipos = Arrays.copyOf(equipos, equipos.length + 1);
 			equipos[equipos.length - 1] = new Equipo(inventario, disponibilidad, codigo, estado);
 
 		}
+		//Si es true es porque ya existe un equipo con ese codigo entonces exception
 	}
 
 	public void eliminarEquipo(String codigoEquipo) {
+		if(verificarEquipo(codigoEquipo)==true) {
 		int i = 0;
 		while (i < equipos.length) {
 			if (codigoEquipo == equipos[i].getCodigo()) {
@@ -90,7 +124,8 @@ public class Sala {
 			}
 			i++;
 		}
-		//No se encontro un equipo con ese codigo. Por lo que no se pudo eliminar
+		}
+		//Si false, no se encontro un equipo con ese codigo. Por lo que no se pudo eliminar
 	}
 
 	public boolean verificarMedicamento(String id) {
@@ -106,9 +141,9 @@ public class Sala {
 	}
 
 
-	public void addMedicamento(Medicamento medicamento, String nombre, String id, Date fechaVencimiento,
+	public void addMedicamento(String nombre, String id, Date fechaVencimiento,
 			Date fechaCompra, boolean disponibilidad, double cantidad) {
-		if (verificarMedicamento(medicamento.getId()) == false) {
+		if (verificarMedicamento(id) == false) {
 			medicamentos = Arrays.copyOf(medicamentos, medicamentos.length + 1);
 			medicamentos[medicamentos.length - 1] = new Medicamento(nombre, id, fechaVencimiento, fechaCompra,
 					disponibilidad, cantidad);
@@ -117,6 +152,7 @@ public class Sala {
 	}
 
 	public void eliminarMedicamento(String idMedicamento) {
+		if(verificarMedicamento(idMedicamento)==true) {
 		int i = 0;
 		while (i < medicamentos.length) {
 			if (idMedicamento == medicamentos[i].getId()) {
@@ -126,6 +162,7 @@ public class Sala {
 			}
 			i++;
 		}
-		//No se encontro un medicamento con ese Id, por lo que no se pudo eliminar.
+		}
+		//Si false, no se encontro un medicamento con ese Id, por lo que no se pudo eliminar.
 	}
 }
