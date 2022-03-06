4091f93f081bfb0b00febb5e54c7917d4b9d1303
Juan Pablo Dìaz Correa <juanipis@gmail.com>
2022-03-06 17:19:31 -0500

    Ventana inicio nueva

--- Hospital/src/hospital/VentanaInicio.java
+++ Hospital/src/hospital/VentanaInicio.java
@@ -40,18 +40,7 @@ public class VentanaInicio extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-
-					VentanaInicio ventanaInicio = new VentanaInicio(new Hospital());
-					ventanaInicio.setVisible(true);
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		
 	}
 
 	/**
