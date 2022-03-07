99599dc0e18ff8c2663f287e52172ef4c97c889e
PedroJuanHenaoVelez <pedro.henao79@eia.edu.co>
2022-03-07 13:39:30 -0500

    Update VentanaPersonalEnfermeros.java

--- Hospital/src/hospital/VentanaPersonalEnfermeros.java
+++ Hospital/src/hospital/VentanaPersonalEnfermeros.java
@@ -7,6 +7,7 @@ import java.awt.SystemColor;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -48,67 +49,20 @@ public VentanaPersonalEnfermeros() {
 	 */
 	private void initialize() {
 
-		setBounds(100, 100, 930, 826);
+		String texto = "";
+		
+		Medico[] medicos = Main.getHospital().getMedicos();
+		for(int x = 0; x < medicos.length; x++) {
+			texto = texto + medicos[x].toString() + "\n";
+		}
+		System.out.print(texto);
+	
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		getContentPane().setLayout(null);
-		setResizable(false);
-
-		txtEnfermeros = new JTextField();
-		txtEnfermeros.setFont(new Font("Verdana", Font.BOLD, 34));
-		txtEnfermeros.setBackground(SystemColor.menu);
-		txtEnfermeros.setBorder(null);
-		txtEnfermeros.setText("Enfermeros");
-		txtEnfermeros.setHorizontalAlignment(SwingConstants.CENTER);
-		txtEnfermeros.setBounds(10, 11, 894, 58);
-		getContentPane().add(txtEnfermeros);
-		txtEnfermeros.setColumns(10);
-		txtEnfermeros.setEditable(false);
-
-		JTextField ins = new JTextField();
-		ins.setBorder(null);
-		ins.setBackground(SystemColor.menu);
-		getContentPane().add(ins);
-		ins.setBounds(415, 80, 400, 23);
-		ins.setVisible(true);
-		ins.setEditable(false);
-		ins.setText("Porfavor ingrese los datos correctos y escoja que desea hacer con ellos");
-		
-		JTextArea listaEnfermeros = new JTextArea();
-		listaEnfermeros.setBounds(10, 80, 400, 379);
-		getContentPane().add(listaEnfermeros);
-		listaEnfermeros.setEditable(false);
-		listaEnfermeros.setText(Arrays.toString(Main.hp.getEnfermeros()));
-		
-		JTextArea datosEnfermero = new JTextArea();
-		datosEnfermero.setBounds(415, 115, 400, 23);
-		getContentPane().add(datosEnfermero);
-		datosEnfermero.setEditable(true);
-		
-		JButton btnAddEnfermero = new JButton("Agregar");
-		btnAddEnfermero.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//Main.hp.addMedico(datosMedico.getText());
-				listaEnfermeros.setText(Arrays.toString(Main.hp.getEnfermeros()) + datosEnfermero.getText());
-			}
-		});
-		btnAddEnfermero.setBounds(415, 150, 89, 30);
-		getContentPane().add(btnAddEnfermero);
-		
-		JButton btnBorrarEnfermero = new JButton("Eliminar");
-		btnBorrarEnfermero.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					Main.hp.eliminarEnfermero(datosEnfermero.getText());
-				} catch (NoExistePersonal | IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				datosEnfermero.getText();
-				listaEnfermeros.setText(Arrays.toString(Main.hp.getEnfermeros()));
-			}
-		});
-		btnBorrarEnfermero.setBounds(415, 180, 89, 30);
-		getContentPane().add(btnBorrarEnfermero);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		
 		JButton btnNewButton = new JButton("Volver");
@@ -117,9 +71,20 @@ public VentanaPersonalEnfermeros() {
 				VentanaInicio inicio = new VentanaInicio();
 				inicio.setVisible(true);
 				dispose();
+				
 			}
 		});
-		btnNewButton.setBounds(446, 600, 89, 23);
-		getContentPane().add(btnNewButton);
+		btnNewButton.setBounds(167, 157, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(15, 16, 398, 138);
+		contentPane.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
+		textArea.setText(texto);
+
 	}
 }
